65cbe045c34784502d77526b3ac1120663cbacd0
jalejandrosandoval <jsandoval5@udi.edu.co>
2021-10-21 21:44:56 -0500

    Manejo de variables básicas JS.

---
+++ Primeros_Pasos_JS_HTML_CSS_21_10_2021/main.js
@@ -0,0 +1,48 @@
+// Datos String
+let nombre = 'Jhon Alejandro Sandoval Miranda';
+console.log(nombre);
+console.log(Object(nombre));
+
+// Dato Number
+let edad = 23;
+console.log(edad);
+console.log(Object(edad));
+
+// Dato Number
+let altura = 1.94;
+console.log(altura);
+console.log(Object(altura));
+
+// Dato boolean
+let soyProfesor = false;
+console.log(soyProfesor);
+console.log(Object(soyProfesor));
+
+// Datos null
+let pelo = null;
+console.log(pelo);
+console.log(Object(pelo));
+
+// Dato undefined
+let brazos; 
+console.log(brazos);
+console.log(Object(brazos));
+
+// Datos Array
+let pasatiempos = ["Volleyball", "Viajar", "leer"];
+console.log(pasatiempos);
+
+// Datos Array
+let contacto = [];
+contacto['Celular'] = 3163072295;
+contacto['email'] = "jhonsandovalmiranda@gmail.com";
+console.log(contacto);
+
+// Datos Objeto
+let dirrecion = {
+    calle: "200A",
+    numero: "19-12",
+    ciudad: "Floridablanca",
+    departamento: "Santander"
+};
+console.log(dirrecion);
